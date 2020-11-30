7b3cf8f87bd3169ea5cb43aeeef6b8110a6e267c
gabriel conejo <gacova95@gmail.com>
2020-11-30 17:39:26 -0600

    inicio de la interfaz

--- src/com/tec/Main.java
+++ src/com/tec/Main.java
@@ -1,8 +1,21 @@
 package com.tec;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+public class Main extends Application{
 
-public class Main {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("ui/app.fxml"));
+        primaryStage.setTitle("LogoTec");
+        primaryStage.setScene(new Scene(root, 1080, 720));
+        primaryStage.show();
+    }
+
 
     public static void main(String[] args) {
-	// write your code here
+        launch(args);
     }
 }
---
+++ src/com/tec/ui/UIEngine.java
@@ -0,0 +1,4 @@
+package com.tec.ui;
+
+public class UIEngine {
+}
